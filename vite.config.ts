5a6813f127138cc76c376613c981464704ad6376
kmatic1984 <124759145+kmatic1984@users.noreply.github.com>
2026-02-17 08:21:02 +0100

    Create Vite configuration for React

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Additional configurations can be added here
-})
+  server: {
+    port: 3000,
+  },
+});
